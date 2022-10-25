9775de6ed2d63e84ff7b1ae447934ccd0dc67e5f
willis <116005508+willllis@users.noreply.github.com>
2022-10-25 11:43:41 +0800

    Add files via upload

---
+++ printBigE.py
@@ -0,0 +1,21 @@
+Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:48:03) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('''
+EEEEEEEEEEEEE
+E
+E
+EEEEEEEEEEEEE
+E
+E
+EEEEEEEEEEEEE
+''')
+
+EEEEEEEEEEEEE
+E
+E
+EEEEEEEEEEEEE
+E
+E
+EEEEEEEEEEEEE
+
+>>> 
